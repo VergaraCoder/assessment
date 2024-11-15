f332474b82e1a5b9d314d51b5e4d2788727ba843
vergaraCoder <jhonatanvergara072@gmail.com>
2024-11-15 11:31:35 -0500

    feat: base project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Credentials } from './common/db/db.config';
+import { RoleModule } from './role/role.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:".env"
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useClass:Credentials
+    }),
+    RoleModule,
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
